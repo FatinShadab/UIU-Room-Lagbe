5c162530d496380e45922c906b8a54ba7d3ec706
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2024-09-30 15:06:40 +0600

    Update homePage.py
    Now HomePage will extend BasePage

--- src/core/pages/homePage.py
+++ src/core/pages/homePage.py
@@ -1,31 +1,16 @@
-from flet import View, NotchShape, FloatingActionButtonLocation
-
-from components.navigation import AppBarBtm
-from components.navigation import Header
+from .base import BasePage
+from flet import Text, FloatingActionButtonLocation
 from components.buttons import ScannerBtn
+from utils.config import COLOR_SCHEMA
 
-from utils.config import APP_NAME, COLOR_SCHEMA
 
-
-class HomePage(View):
+class HomePage(BasePage):
     def __init__(self, route, **kwargs):
         super().__init__(route=route, **kwargs)  # Make sure route is passed here
         
-        self.appbar = Header(
-                APP_NAME,
-                bgcolor=COLOR_SCHEMA["ORANGE"],
-                automatically_imply_leading=False
-        )
+        self.controls = [
+                Text("Home Page")
+        ]
         
-        self.bottom_appbar = AppBarBtm(
-                bgcolor=COLOR_SCHEMA["ORANGE"],
-                shape=NotchShape.CIRCULAR,
-        )
-        
-        self.floating_action_button = ScannerBtn(bgcolor=COLOR_SCHEMA["WHITE"], foreground_color=COLOR_SCHEMA["ORANGE"])
+        self.floating_action_button = ScannerBtn(role=0, bgcolor=COLOR_SCHEMA["WHITE"], foreground_color=COLOR_SCHEMA["ORANGE"])
         self.floating_action_button_location = FloatingActionButtonLocation.CENTER_DOCKED
-        
-        self.controls = []
-        
-        self.horizontal_alignment = self.vertical_alignment = "center"
-        self.spacing = 10
