9f031df222571edf12fed0a72426b551b664017c
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2024-09-30 15:06:52 +0600

    Create scanPage.py
    ScanPage will extend BasePage

---
+++ src/core/pages/scanPage.py
@@ -0,0 +1,16 @@
+from .base import BasePage
+from flet import Text, FloatingActionButtonLocation
+from components.buttons import ScannerBtn
+from utils.config import COLOR_SCHEMA
+
+
+class ScanPage(BasePage):
+    def __init__(self, route, **kwargs):
+        super().__init__(route=route, **kwargs)  # Make sure route is passed here
+        
+        self.controls = [
+            Text("Scan Page 2")
+        ]
+        
+        self.floating_action_button = ScannerBtn(role=1, bgcolor=COLOR_SCHEMA["WHITE"], foreground_color=COLOR_SCHEMA["ORANGE"])
+        self.floating_action_button_location = FloatingActionButtonLocation.CENTER_DOCKED
