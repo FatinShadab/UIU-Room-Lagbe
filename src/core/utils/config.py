783ff4a58e3dc345e5da681510906e4baf818852
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2024-09-30 15:05:31 +0600

    Update config.py

--- src/core/utils/config.py
+++ src/core/utils/config.py
@@ -1,7 +1,13 @@
 from flet import colors
 
 APP_NAME = 'UIU Room Lagbe!'
+
 COLOR_SCHEMA = {
     "ORANGE": colors.ORANGE_900,
     "WHITE": colors.WHITE,
+}
+
+ROUTES = {
+    "Home": "/",
+    "Scan": "/scan",
 }
