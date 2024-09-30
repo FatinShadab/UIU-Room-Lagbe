3f1e771ff84b6ccdc8f3ea4ac86740ec1734ff55
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2024-09-30 12:07:31 +0600

    Create __init__.py
    created components module to store all reusable flet UI elements

---
+++ src/core/components/__init__.py
@@ -0,0 +1,6 @@
+"""
+### This package encompasses all the custom component code for the application.
+### It serves as a centralized module for defining and managing reusable UI components.
+
+author: @FatinShadab
+"""
