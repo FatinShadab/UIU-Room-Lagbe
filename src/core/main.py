ebafa9eddb127267582545f54e83ba3c1458405d
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2024-09-30 13:38:01 +0600

    Update main.py
    cleaned the driver file

--- src/core/main.py
+++ src/core/main.py
@@ -1,31 +1,24 @@
 import flet as ft
-
-from utils.config import APP_NAME, COLOR_SCHEMA
-
-from components.navigation import AppBarBtm
-from components.navigation import Header
-from components.buttons import ScannerBtn
-
+from app import App
 
 def main(page: ft.Page):
-    page.horizontal_alignment = page.vertical_alignment = "center"
-    page.floating_action_button = ScannerBtn(bgcolor=COLOR_SCHEMA["WHITE"], foreground_color=COLOR_SCHEMA["ORANGE"])
-    page.floating_action_button_location = ft.FloatingActionButtonLocation.CENTER_DOCKED
-
-    page.appbar = Header(
-        APP_NAME,
-        bgcolor=COLOR_SCHEMA["ORANGE"],
-        automatically_imply_leading=False
-    )
     
-    page.bottom_appbar = AppBarBtm(
-        bgcolor=COLOR_SCHEMA["ORANGE"],
-        shape=ft.NotchShape.CIRCULAR,
-    )
-
-    page.update()
+    try:
+        app = App(page)
+        
+        try:
+            app.run()
+        except Exception as e:
+            print("[IN App.run()] " + str(e))
+            app.page.show_snackbar("An error occurred. Please try again later.")
+            app.page.update()
+            
+    except Exception as e:
+        print("[IN App.__init__()] " + str(e))
+        page.show_snackbar("An error occurred. Please try again later.")
+        page.update()
 
 
 if __name__ == "__main__":
-    ft.app(main)
+    ft.app(target=main, assets_dir="assets")
     
